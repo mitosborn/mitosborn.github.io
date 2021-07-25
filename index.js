38e66b05bed84c871fc34994acd73d447ad11417
Mitchell Osborn <mitosborn@icloud.com>
2021-07-25 16:16:39 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // init Masonry
+    var $grid = $('.grid').masonry({
+        // options...
+    });
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress( function() {
+        $grid.masonry('layout');
+    });
+  });
